13d85bd21057ef1e6861fd9f0b80ce72bae8d35e
Richard Diz <rich.b.diz@gmail.com>
2019-12-29 23:51:22 -0500

    myapp for clarity

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore, \
-    UserMixin, RoleMixin, login_required
+from flask_security import Security, SQLAlchemyUserDatastore
 
 db = SQLAlchemy()
 security = Security()
 
 # Create app
 def create_app():
-    app = Flask(__name__)
-    app.config['DEBUG'] = True
-    app.config['SECRET_KEY'] = 'super-secret'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECURITY_PASSWORD_SALT'] = 'onetwothreefourfive'
+    myapp = Flask(__name__)
+    myapp.config['DEBUG'] = True
+    myapp.config['SECRET_KEY'] = 'super-secret'
+    myapp.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+    myapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    myapp.config['SECURITY_PASSWORD_SALT'] = 'onetwothreefourfive'
 
-    db.init_app(app)
+    db.init_app(myapp)
 
     from app.models import User, Role
     user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-    security.init_app(app, user_datastore)
+    security.init_app(myapp, user_datastore)
 
     from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
+    myapp.register_blueprint(main_bp)
 
 
     # debug only for testing.
-    app_context = app.app_context()
+    app_context = myapp.app_context()
     app_context.push()
     db.create_all()
     user_datastore.create_user(email='matt@nobien.net', password='password')
     db.session.commit()
 
-    return app
+    return myapp
