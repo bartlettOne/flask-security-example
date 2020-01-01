1e4660c059020bf9b5dc04a10d59ed0cae31088c
Richard Diz <rich.b.diz@gmail.com>
2019-12-31 19:31:29 -0500

    set default user role but with a bad technique

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore
 from flask_mail import Mail
+from flask_security.signals import user_registered
 
 db = SQLAlchemy()
 security = Security()
@@ -45,6 +46,19 @@ def create_app():
     from app.main import bp as main_bp
     myapp.register_blueprint(main_bp)
 
+# This is junk. This should be in the security blueprint, but the "myapp" variable is not
+#   available there, so I had to register it here.
+# I thought I could put connect_via(bp), but that didn't work
+# Here are some hints for fixing this
+# https://stackoverflow.com/questions/16163139/catch-signals-in-flask-blueprint
+# https://stackoverflow.com/questions/57866541/how-to-get-user-registered-signal-in-flask-user
+# https://stackoverflow.com/questions/17146724/setting-a-default-role-in-flask-security?rq=1
+
+    @user_registered.connect_via(myapp)
+    def user_registered_sighandler(app, user, confirm_token):
+        default_role = user_datastore.find_role("user")
+        user_datastore.add_role_to_user(user, default_role)
+        db.session.commit()
 
     # debug only for testing.
     app_context = myapp.app_context()
@@ -52,7 +66,7 @@ def create_app():
     db.create_all()
         # Create the Roles "admin" and "end-user" -- unless they already exist
     user_datastore.find_or_create_role(name='admin', description='Administrator')
-    user_datastore.find_or_create_role(name='end-user', description='End user')
+    user_datastore.find_or_create_role(name='user', description='End user')
     
     user_datastore.create_user(email='someone@example.com', password='password')
     user_datastore.create_user(email='admin@example.com', password='password')
@@ -61,8 +75,9 @@ def create_app():
 
     # Give one User has the "end-user" role, while the other has the "admin" role. (This will have no effect if the
     # Users already have these Roles.) Again, commit any database changes.
-    user_datastore.add_role_to_user('someone@example.com', 'end-user')
+    user_datastore.add_role_to_user('someone@example.com', 'user')
     user_datastore.add_role_to_user('admin@example.com', 'admin')
     db.session.commit()
 
     return myapp
+   
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,10 +1,18 @@
-from app import db
+from app import db#, user_datastore
 from flask_security import login_required
 from app.main import bp
 from flask import render_template
+#from flask_security.signals import user_registered
 
 # Views
 @bp.route('/')
 @login_required
 def home():
     return render_template('index.html')
+
+# # @user_registered.connect_via(current_app)
+# def user_registered_sighandler(app_sender, user, confirm_token):
+#     default_role = user_datastore.find_role("user")
+#     user_datastore.add_role_to_user(user, default_role)
+#     db.session.commit()
+
